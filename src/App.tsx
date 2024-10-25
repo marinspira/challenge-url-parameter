8c7c6a639b128e119b4e198f58ca0f6308f8ac7b
Maria Eduarda <102706722+marinspira@users.noreply.github.com>
2024-10-25 19:57:06 +0000

    First commit

--- src/App.tsx
+++ src/App.tsx
@@ -8,13 +8,6 @@ import {
   useParams,
 } from "react-router-dom";
 
-/* Instructions 
-  Create two Routes so that the given navigation works.
-
-  When the user navigates to /Ram, they should see "Student: Ram".
-  When the user navigates to /ids/1, they should see "Ids : 1".
-*/
-
 const Employee = () => {
   const { name } = useParams();
   return (
